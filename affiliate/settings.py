ea94bf37ec39e7aeb5d24442481736acd6283ca8
Utibeabasi Peter Umanah <utibeabasi@users-MacBook-Pro.local>
2021-06-10 21:35:41 +0100

    first commit

--- affiliate/settings.py
+++ affiliate/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$3r@+jknj^8ps7g4mn!_6+6sn7j$c=mp69k&(qg9ozoj5+c=pr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['utibestore.herokuapp.com']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'root'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
