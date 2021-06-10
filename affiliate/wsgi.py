cd9e7055819ad2cd90ba16e2cd82d902f13cca82
Utibeabasi Peter Umanah <utibeabasi@users-MacBook-Pro.local>
2021-06-10 21:40:02 +0100

    first commit

--- affiliate/wsgi.py
+++ affiliate/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
+
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'affiliate.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root= BASE_DIR / 'media')
